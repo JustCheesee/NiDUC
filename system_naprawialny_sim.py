73a5e6c91139cbbe91a1151ff3d246d256b15208
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-03-18 13:12:03 +0100

    Add files via upload

---
+++ system_naprawialny_sim.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import sys
+import random
+import numpy as np
+import matplotlib.pyplot as plt
+
+def element_naprawialny(l, mi, t):							#l = lambda
+	czas = 0.0
+	stan = 0
+	stanLista = [0]
+	czasLista = [0.0]
+
+	print("Czas: ", czas, " Stan: ", stan)
+
+	while czas < t:
+		s = stan;											# stan aktualny
+		if stan == 0:										# element sprawny - wystąpi uszkodzenie
+			czas = czas + np.random.exponential(l);			# losowanie chwili uszkodzenia
+			czasLista.append(czas)
+			stanLista.append(stan)
+			stan = 1;										# zmiana stanu na 1 (element uszkodzony)
+		else:
+			czas = czas + np.random.exponential(mi);		# losowanie chwili zakończenia naprawy
+			czasLista.append(czas)
+			stanLista.append(stan)
+			stan = 0;
+		print("Czas: ", czas, " Stan: ", stan)
+		stanLista.append(stan)
+		czasLista.append(czas)
+
+	plt.plot(czasLista, stanLista)
+	# plt.axis([0, 10000, 0, 1.2])
+	plt.xlim(0, 10000)
+	plt.show()
+
+def main():
+	element_naprawialny(1000, 100, 10000)
+		
+if __name__ == '__main__':
+	main()
