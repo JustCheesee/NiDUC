14f8e495574b8e09512e44806a0497ae06ba3af5
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-03-21 11:47:20 +0100

    Add files via upload

--- system_naprawialny_sim.py
+++ system_naprawialny_sim.py
@@ -4,36 +4,49 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-def element_naprawialny(l, mi, t):							#l = lambda
-	czas = 0.0
-	stan = 0
-	stanLista = [0]
-	czasLista = [0.0]
+class Element_naprawialny:
+	def __init__(self, lam, mi, t):
+		self.lam = lam
+		self.mi = mi
+		self.t = t
 
-	print("Czas: ", czas, " Stan: ", stan)
+	def symulacja(self):											#l = lambda
+		czas = 0.0
+		stan = 0
+		stanLista = [0]
+		czasLista = [0.0]
+		gotowosc = 0.0
 
-	while czas < t:
-		s = stan;											# stan aktualny
-		if stan == 0:										# element sprawny - wystąpi uszkodzenie
-			czas = czas + np.random.exponential(l);			# losowanie chwili uszkodzenia
-			czasLista.append(czas)
-			stanLista.append(stan)
-			stan = 1;										# zmiana stanu na 1 (element uszkodzony)
-		else:
-			czas = czas + np.random.exponential(mi);		# losowanie chwili zakończenia naprawy
-			czasLista.append(czas)
-			stanLista.append(stan)
-			stan = 0;
 		print("Czas: ", czas, " Stan: ", stan)
-		stanLista.append(stan)
-		czasLista.append(czas)
 
-	plt.plot(czasLista, stanLista)
-	plt.xlim(0, t)
-	plt.show()
+		while czas < self.t:
+			s = stan;												# stan aktualny
+			if stan == 0:											# element sprawny - wystąpi uszkodzenie
+				r = np.random.exponential(self.lam)
+				czas = czas + r										# losowanie chwili uszkodzenia
+				gotowosc = gotowosc + r
+				czasLista.append(czas)
+				stanLista.append(stan)
+				stan = 1;											# zmiana stanu na 1 (element uszkodzony)
+			else:
+				czas = czas + np.random.exponential(self.mi);		# losowanie chwili zakończenia naprawy
+				czasLista.append(czas)
+				stanLista.append(stan)
+				stan = 0;
+			print("Czas: ", czas, " Stan: ", stan)
+			stanLista.append(stan)
+			czasLista.append(czas)
+
+		print(gotowosc/czas)
+
+		plt.plot(czasLista, stanLista)
+		plt.xlim(0, self.t)
+		plt.show()
+	
 
 def main():
-	element_naprawialny(1000, 100, 10000)
+	e1 = Element_naprawialny(20000, 24, 100000)
+	e1.symulacja()
 		
 if __name__ == '__main__':
 	main()
