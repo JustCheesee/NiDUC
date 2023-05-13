57eccc6470cd30424768e3e436dfd206f1c379c4
Michal <smandzich.michal@gmail.com>
2023-05-13 21:52:26 +0200

    Poprawione Analysis.py

--- Analysis.py
+++ Analysis.py
@@ -3,18 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # tu podac sciezku do pliku z ktorego checmy obliczyc procent sprawnosci dzialania systemu
-filename = "output/simulation_0/online_offline_simulation_0.txt"
-active_time = 0
-total_time = 0
+data = np.loadtxt("output/simulation_0/online_offline_simulation_0.txt", delimiter=";")
+total_time = 100000*0.00365
 
-with open(filename, "r") as file:
-    for line in file:
-        time, status = line.strip().split(";")
-        if int(status) == 1:
-            active_time += 1
-        total_time += 1
+# Przeliczenie czasów na dni
+data[:, 0] = data[:, 0] * 0.00365
 
-active_percentage = active_time / total_time * 100
+# Obliczenie czasów działania i czasów awarii
+downtime = data[:, 0][data[:, 1] == 1]
+uptime = data[:, 0][data[:, 1] == 0]
+
+downtime_downtime = uptime
+for x in range(len(downtime)):
+    downtime_downtime[x] = uptime[x] - downtime[x]
+# Obliczenie całkowitego czasu i dostępności
+downtime_sum = np.sum(downtime_downtime)
+
+active_percentage = 100 - (downtime_sum / total_time) * 100
 inactive_percentage = 100 - active_percentage
 
 labels = ['Jest Sprawny', 'Nie Jest Sprawny']
