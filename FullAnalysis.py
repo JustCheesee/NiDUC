6eccf249704a1be15bea3b7ecd78699d6d02585f
Xololt <105434979+xololt-dev@users.noreply.github.com>
2023-05-15 20:14:17 +0200

    Dodano timeline serwerowni oraz dystrybucję długości dostępności serwerowni

--- FullAnalysis.py
+++ FullAnalysis.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 from scipy.stats import poisson
 
 # tu podac sciezku do pliku z ktorego checmy obliczyc procent sprawnosci dzialania systemu
-data = np.loadtxt("output/simulation_2/online_offline_simulation_2.txt", delimiter=";")
+data = np.loadtxt("output/simulation_1/online_offline_simulation_1.txt", delimiter=";")
 total_time = 100000*0.00365
 
 # Przeliczenie czasów na dni
@@ -24,6 +24,25 @@ for x in range(len(downtime)):
 downtime_sum = np.sum(downtime_downtime)
 active_percentage = 100 - (downtime_sum / total_time) * 100
 inactive_percentage = 100 - active_percentage
+
+workingTime = []
+lastDefect = 0
+stanLista = [0]
+czasLista = [0.0]
+
+for d in data[:,:]:
+	czasLista.append(d[0])
+	czasLista.append(d[0])
+	if d[1] == 0:
+		lastDefect = d[0]
+		stanLista.append(1)
+	else:
+		workingTime.append(d[0] - lastDefect)
+		stanLista.append(0)
+	stanLista.append(d[1])
+workingTime.append(365 - lastDefect)
+stanLista.append(0)
+czasLista.append(365)
 
 labels = ['Jest Sprawny', 'Nie Jest Sprawny']
 sizes = [active_percentage, inactive_percentage]
@@ -52,6 +71,14 @@ print(f"Najkrótszy czas działania: {min_val:.2f}")
 print(f"Najdłuższy czas działania: {max_val:.2f}")
 print(f"Poziom ryzyka w %: {risk:.5f}")
 
+# Ogólny stan serwerowni w trakcie roku
+plt.plot(czasLista, stanLista)
+plt.title("Linia czasu dostępności serwera")
+plt.xlabel("Czas (dni)")
+plt.ylabel("Stan")
+plt.xlim(0, total_time)
+plt.show()
+
 # Empiryczna funkcja dystrybucji
 n = len(uptime)
 x = np.sort(uptime)
@@ -70,9 +97,15 @@ plt.title('Dystrybucja czasu awarii')
 plt.show()
 
 # Histogram naprawy
-plt.hist(working_hours, bins=24, range=(0, 24), color='red', alpha=0.7)
-plt.xlabel('Czas przerwy (godzin)')
-plt.ylabel('Częstotliwość')
-plt.title('Dystrybucja czasu sprawności')
-plt.show()
+# plt.hist(working_hours, bins=24, range=(0, 24), color='red', alpha=0.7)
+# plt.xlabel('Czas przerwy (godzin)')
+# plt.ylabel('Częstotliwość')
+# plt.title('Dystrybucja czasu sprawności')
+# plt.show()
 
+# Histogram czasu działania
+plt.hist(workingTime, bins = 90)
+plt.title("Czas działania serwera")
+plt.xlabel("Czas (dni)")
+plt.ylabel("Częstotliwość")
+plt.show()
